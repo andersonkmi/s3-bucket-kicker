4bfc7221cdc734b94b715198cab839723b1e4e16
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-09-26 12:52:34 +0200

    more code

--- src/main/java/org/codecraftlabs/kikker/util/FileUtil.java
+++ src/main/java/org/codecraftlabs/kikker/util/FileUtil.java
@@ -4,20 +4,20 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FileUtil {
-    public List<String> listFiles(String folder, String fileExtension) {
+    public Set<String> listFiles(String folder, String fileExtension) {
         var rootFolder = new File(folder);
         if (!rootFolder.exists() || !rootFolder.isDirectory()) {
-            return Collections.emptyList();
+            return Collections.emptySet();
         }
 
         var matchedFiles = rootFolder.list((dir, name) -> name.endsWith(fileExtension));
         return matchedFiles != null ?
-                Arrays.stream(matchedFiles).map(item -> prependFolderName(folder, item)).collect(Collectors.toList()) :
-                Collections.emptyList();
+                Arrays.stream(matchedFiles).map(item -> prependFolderName(folder, item)).collect(Collectors.toSet()) :
+                Collections.emptySet();
     }
 
     private String prependFolderName(@Nonnull final String folderName, @Nonnull final String fileName) {
