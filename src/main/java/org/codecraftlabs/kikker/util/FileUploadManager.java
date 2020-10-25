3a332dff9a16c1a66ec9558d0fb6e2ab5a1fa002
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-10-25 22:35:05 +0100

    more code

--- src/main/java/org/codecraftlabs/kikker/util/FileUploadManager.java
+++ src/main/java/org/codecraftlabs/kikker/util/FileUploadManager.java
@@ -1,9 +1,18 @@
 package org.codecraftlabs.kikker.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FileUploadManager {
+    private static final Logger logger = LogManager.getLogger(FileUploadManager.class);
+
     private final String controlFile;
     private Set<String> uploadedFiles;
 
@@ -12,5 +21,18 @@ public class FileUploadManager {
         uploadedFiles = new HashSet<>();
 
         // Loads the upload control file
+        File uploadControlFile = new File(controlFile);
+        if (uploadControlFile.exists()) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(uploadControlFile.getName()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    uploadedFiles.add(line);
+                }
+                reader.close();
+            } catch (IOException exception) {
+                logger.warn("Error when opening the control file", exception);
+            }
+        }
     }
 }
